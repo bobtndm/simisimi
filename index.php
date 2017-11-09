06d852b6cdfec4999788c33394a6692277d560cf
bobtndm <30938318+bobtndm@users.noreply.github.com>
2017-11-09 19:56:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if($message['type']=='sticker')
 							'messages' => array(
 								array(
 										'type' => 'text',									
-										'text' => 'Terima Kasih Stikernya.'										
+										'text' => 'Minta dong, stikernya.'										
 									
 									)
 							)
@@ -49,7 +49,7 @@ if($url['result'] == 404)
 							'messages' => array(
 								array(
 										'type' => 'text',					
-										'text' => 'Mohon Gunakan Bahasa Indonesia Yang Benar :D.'
+										'text' => 'Apasih.'
 									)
 							)
 						);
@@ -66,7 +66,7 @@ if($url['result'] != 100)
 							'messages' => array(
 								array(
 										'type' => 'text',					
-										'text' => 'Maaf '.$profil->displayName.' Server Kami Sedang Sibuk Sekarang.'
+										'text' => 'Hai '.$profil->displayName.' Aku Capek, jawab kamu trus:(.'
 									)
 							)
 						);
